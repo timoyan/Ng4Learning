ec66efdb33ff4596079b284d96910ec6dbe80755
TimoYan <tyan022@nam.ad.pwcinternal.com>
2017-11-02 17:56:18 +0800

    redesign mobile layout

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../service/auth.service';
-import { Router } from '@angular/router';
+import { Router, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -12,7 +12,11 @@ export class HeaderComponent implements OnInit {
   constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
-        
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        console.log("Header url subscribe : " + event.url);
+      }
+    });
   }
 
   onLogOut(e: MouseEvent) {
@@ -20,5 +24,11 @@ export class HeaderComponent implements OnInit {
     this.router.navigate(['/login']);
     e.stopPropagation();
     return false;
+  }
+
+  navgate_onChange(e) {
+    let navigateLink = e.target.value;
+    console.log(navigateLink);
+    this.router.navigate([navigateLink]);
   }
 }
